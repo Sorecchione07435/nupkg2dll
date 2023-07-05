4fadc32c94883c15286dbd80e51abd916ec5c2ed
Sorecchione07435 <111366201+Sorecchione07435@users.noreply.github.com>
2023-07-05 15:27:56 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(155, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter your Nupkg package file:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(173, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(362, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(541, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(42, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(589, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Extract"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(675, 41)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "nupkg2dll - NUPKG to DLL Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.IO.Compression
+Public Class Form1
+    Dim CloseAfterFinish As Boolean = False
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim OFD As New OpenFileDialog
+        OFD.Filter = "Nuget Package File|*.nupkg"
+        OFD.FileName = Nothing
+        If OFD.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = OFD.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If TextBox1.Text = Nothing Then
+            MsgBox("Enter a nupkg file!", MsgBoxStyle.Exclamation, "Warning") : Exit Sub
+        End If
+
+        MkDir(Application.StartupPath & "\temp")
+        If My.Computer.FileSystem.DirectoryExists(Application.StartupPath & "\extracted") = False Then
+            MkDir(Application.StartupPath & "\extracted")
+        End If
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        ZipFile.ExtractToDirectory(TextBox1.Text, Application.StartupPath & "\temp")
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        For Each f In Directory.GetFiles(Application.StartupPath & "\temp", "*.dll", SearchOption.AllDirectories)
+            If File.Exists(f) Then
+                File.Copy(f, Path.Combine(Application.StartupPath & "\extracted", Path.GetFileName(f)), True)
+            End If
+
+        Next
+        My.Computer.FileSystem.DeleteDirectory(Application.StartupPath & "\temp", FileIO.DeleteDirectoryOption.DeleteAllContents)
+        MsgBox("Conversion Successful")
+        Process.Start(Application.StartupPath & "\extracted")
+
+        If CloseAfterFinish = True Then
+            End
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim args = Environment.GetCommandLineArgs()
+        If args.Length > 1 Then
+            If args(1).ToString.EndsWith(".nupkg") = True Then
+                TextBox1.Text = args(1)
+                CloseAfterFinish = True
+                Button2.PerformClick()
+            Else
+                MsgBox("this file is not supported!, check that it is not damaged or has the "".nupkg"" extension and try again", MsgBoxStyle.Critical, "Error")
+                End
+            End If
+
+        End If
+    End Sub
+End Class
